00107baa5a57be6543212e89de0b752dbc26283f
Srinivasgolla15 <lsrinivas0768@gmail.com>
2024-10-24 11:28:20 +0530

    created  reactjs file

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export function Login(){
+  return <div>
+    <h1> login to lms</h1><br/>
+    <label>Email</label><br/>
+    <input type ='text' placeholder='enter Email'/>
+  </div>
+}
+
+export default App;
